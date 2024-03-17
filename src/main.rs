2be23a81fa9c0951be16ba3fe0f024c5f4e3858d
Hardhat Chad <hardhatchad@gmail.com>
2024-03-17 14:53:28 +0000

    admin functions

--- src/main.rs
+++ src/main.rs
@@ -190,11 +190,11 @@ async fn main() {
         }
         #[cfg(feature = "admin")]
         Commands::UpdateAdmin(cmd) => {
-            // TODO
+            miner.update_admin(cmd.new_admin).await;
         }
         #[cfg(feature = "admin")]
         Commands::UpdateDifficulty(cmd) => {
-            // TODO
+            miner.update_difficulty(cmd.new_difficulty).await;
         }
     }
 }
@@ -524,6 +524,40 @@ impl<'a> Miner<'a> {
 
         // Sign and send transaction.
         let ix = ore::instruction::initialize(self.signer.pubkey());
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&self.signer.pubkey()));
+        let recent_blockhash = client.get_latest_blockhash().await.unwrap();
+        tx.sign(&[&self.signer], recent_blockhash);
+        match client.send_and_confirm_transaction(&tx).await {
+            Ok(sig) => println!("{:?}", sig),
+            Err(e) => println!("Transaction failed: {:?}", e),
+        }
+    }
+
+    #[cfg(feature = "admin")]
+    async fn update_admin(&self, new_admin: String) {
+        let new_admin = Pubkey::from_str(new_admin.as_str()).unwrap();
+        let client =
+            RpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::processed());
+
+        // Sign and send transaction.
+        let ix = ore::instruction::update_admin(self.signer.pubkey(), new_admin);
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&self.signer.pubkey()));
+        let recent_blockhash = client.get_latest_blockhash().await.unwrap();
+        tx.sign(&[&self.signer], recent_blockhash);
+        match client.send_and_confirm_transaction(&tx).await {
+            Ok(sig) => println!("{:?}", sig),
+            Err(e) => println!("Transaction failed: {:?}", e),
+        }
+    }
+
+    #[cfg(feature = "admin")]
+    async fn update_difficulty(&self, new_difficulty: String) {
+        let new_difficulty = KeccakHash::from_str(new_difficulty.as_str()).unwrap();
+        let client =
+            RpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::processed());
+
+        // Sign and send transaction.
+        let ix = ore::instruction::update_difficulty(self.signer.pubkey(), new_difficulty.into());
         let mut tx = Transaction::new_with_payer(&[ix], Some(&self.signer.pubkey()));
         let recent_blockhash = client.get_latest_blockhash().await.unwrap();
         tx.sign(&[&self.signer], recent_blockhash);
