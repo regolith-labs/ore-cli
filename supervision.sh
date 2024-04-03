88adc3bebba8b30e39bfa3c8c8658ba67dd6a3f3
Bruno Sanguinetti <brunoebarros@gmail.com>
2024-04-03 13:19:50 -0300

    key args to script

--- supervision.sh
+++ supervision.sh
@@ -2,16 +2,18 @@
 
 # Set default values
 DEFAULT_RPC_URL="https://api.mainnet-beta.solana.com"
+DEFAULT_KEY="~/.config/solana/id.json"
 DEFAULT_FEE=1
 DEFAULT_THREADS=4
 
 # Assign arguments with defaults
 RPC_URL=${1:-$DEFAULT_RPC_URL}
-FEE=${2:-$DEFAULT_FEE}
-THREADS=${3:-$DEFAULT_THREADS}
+KEY=${2:-$DEFAULT_KEY}
+FEE=${3:-$DEFAULT_FEE}
+THREADS=${4:-$DEFAULT_THREADS}
 
 # Command and its arguments, with dynamic values
-COMMAND="./target/release/ore --rpc ${RPC_URL} --keypair ~/.config/solana/id.json --priority-fee ${FEE} mine --threads ${THREADS}"
+COMMAND="./target/release/ore --rpc ${RPC_URL} --keypair ${KEY} --priority-fee ${FEE} mine --threads ${THREADS}"
 
 # Loop indefinitely
 while true; do
