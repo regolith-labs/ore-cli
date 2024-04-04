40755961fb2020e987c27ed725fa9844a2baa91f
Hardhat Chad <hardhatchad@gmail.com>
2024-04-04 05:09:10 +0000

    add cu budget and fee to other txs

--- src/register.rs
+++ src/register.rs
@@ -1,7 +1,10 @@
 use solana_client::nonblocking::rpc_client::RpcClient;
-use solana_sdk::{commitment_config::CommitmentConfig, signature::Signer};
+use solana_sdk::{
+    commitment_config::CommitmentConfig, compute_budget::ComputeBudgetInstruction,
+    signature::Signer,
+};
 
-use crate::{utils::proof_pubkey, Miner};
+use crate::{cu_limits::CU_LIMIT_REGISTER, utils::proof_pubkey, Miner};
 
 impl Miner {
     pub async fn register(&self) {
@@ -16,8 +19,10 @@ impl Miner {
 
         // Sign and send transaction.
         println!("Generating challenge...");
+        let cu_limit_ix = ComputeBudgetInstruction::set_compute_unit_limit(CU_LIMIT_REGISTER);
+        let cu_price_ix = ComputeBudgetInstruction::set_compute_unit_price(self.priority_fee);
         let ix = ore::instruction::register(signer.pubkey());
-        self.send_and_confirm(&[ix], false)
+        self.send_and_confirm(&[cu_limit_ix, cu_price_ix, ix], false)
             .await
             .expect("Transaction failed");
     }
