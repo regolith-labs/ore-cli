d6903c671c32dc15a19726dd54704f5c27d5b7d5
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-04-04 12:05:20 -0400

    added automatic loading of config and an optional flag

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,16 @@ struct Args {
         help = "Network address of your RPC provider",
         default_value = "https://api.mainnet-beta.solana.com"
     )]
-    rpc: String,
+    rpc: Option<String>,
+
+    #[clap(
+        global(true),
+        short = 'C',
+        long = "config",
+        id = "PATH",
+        help = "Filepath to config file."
+    )]
+    pub config_file: Option<String>,
 
     #[arg(
         long,
@@ -160,10 +169,29 @@ struct UpdateDifficultyArgs {}
 
 #[tokio::main]
 async fn main() {
-    // Initialize miner.
     let args = Args::parse();
-    let cluster = args.rpc;
-    let miner = Arc::new(Miner::new(cluster.clone(), args.priority_fee, args.keypair));
+
+    // Load the config file from custom path, the default path, or use default config values
+    let cli_config = if let Some(config_file) = &args.config_file {
+        solana_cli_config::Config::load(config_file).unwrap_or_else(|_| {
+            eprintln!("error: Could not find config file `{}`", config_file);
+            std::process::exit(1);
+        })
+    } else if let Some(config_file) = &*solana_cli_config::CONFIG_FILE {
+        solana_cli_config::Config::load(config_file).unwrap_or_default()
+    } else {
+        solana_cli_config::Config::default()
+    };
+
+    // Initialize miner.
+    let cluster = args.rpc.unwrap_or(cli_config.json_rpc_url);
+    let default_keypair = args.keypair.unwrap_or(cli_config.keypair_path);
+
+    let miner = Arc::new(Miner::new(
+        cluster.clone(),
+        args.priority_fee,
+        Some(default_keypair),
+    ));
 
     // Execute user command.
     match args.command {
