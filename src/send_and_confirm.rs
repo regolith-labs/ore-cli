061b4414803b5f08c5620a03d6967a81585f958b
Hardhat Chad <hardhatchad@gmail.com>
2024-04-05 20:37:12 +0000

    increase delay between retries

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -151,7 +151,7 @@ impl Miner {
             stdout.flush().ok();
 
             // Retry
-            std::thread::sleep(Duration::from_millis(200));
+            std::thread::sleep(Duration::from_millis(1000));
             (hash, slot) = client
                 .get_latest_blockhash_with_commitment(CommitmentConfig::confirmed())
                 .await
