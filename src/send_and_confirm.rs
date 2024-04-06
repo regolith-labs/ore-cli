74d07e47eee917532f0dbd6b5264232d35f52b2f
Hardhat Chad <hardhatchad@gmail.com>
2024-04-06 03:53:32 +0000

    update dynamic sim flow

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -20,6 +20,7 @@ use solana_transaction_status::{TransactionConfirmationStatus, UiTransactionEnco
 use crate::Miner;
 
 const RPC_RETRIES: usize = 0;
+const SIMULATION_RETRIES: usize = 4;
 const GATEWAY_RETRIES: usize = 4;
 const CONFIRM_RETRIES: usize = 4;
 
@@ -63,31 +64,57 @@ impl Miner {
 
         // Simulate if necessary
         if dynamic_cus {
-            let sim_res = client
-                .simulate_transaction_with_config(
-                    &tx,
-                    RpcSimulateTransactionConfig {
-                        sig_verify: false,
-                        replace_recent_blockhash: false,
-                        commitment: Some(CommitmentConfig::confirmed()),
-                        encoding: Some(UiTransactionEncoding::Base64),
-                        accounts: None,
-                        min_context_slot: Some(slot),
-                        inner_instructions: false,
-                    },
-                )
-                .await;
-            if let Ok(sim_res) = sim_res {
-                if let Some(units_consumed) = sim_res.value.units_consumed {
-                    let cu_budget_ix = ComputeBudgetInstruction::set_compute_unit_limit(
-                        units_consumed as u32 + 1000,
-                    );
-                    let cu_price_ix =
-                        ComputeBudgetInstruction::set_compute_unit_price(self.priority_fee);
-                    let mut final_ixs = vec![];
-                    final_ixs.extend_from_slice(&[cu_budget_ix, cu_price_ix]);
-                    final_ixs.extend_from_slice(ixs);
-                    tx = Transaction::new_with_payer(&final_ixs, Some(&signer.pubkey()));
+            let mut sim_attempts = 0;
+            'simulate: loop {
+                let sim_res = client
+                    .simulate_transaction_with_config(
+                        &tx,
+                        RpcSimulateTransactionConfig {
+                            sig_verify: false,
+                            replace_recent_blockhash: true,
+                            commitment: Some(CommitmentConfig::confirmed()),
+                            encoding: Some(UiTransactionEncoding::Base64),
+                            accounts: None,
+                            min_context_slot: None,
+                            inner_instructions: false,
+                        },
+                    )
+                    .await;
+                match sim_res {
+                    Ok(sim_res) => {
+                        if let Some(err) = sim_res.value.err {
+                            println!("Simulaton error: {:?}", err);
+                            sim_attempts += 1;
+                            if sim_attempts.gt(&SIMULATION_RETRIES) {
+                                return Err(ClientError {
+                                    request: None,
+                                    kind: ClientErrorKind::Custom("Simulation failed".into()),
+                                });
+                            }
+                        } else if let Some(units_consumed) = sim_res.value.units_consumed {
+                            println!("Dynamic CUs: {:?}", units_consumed);
+                            let cu_budget_ix = ComputeBudgetInstruction::set_compute_unit_limit(
+                                units_consumed as u32 + 1000,
+                            );
+                            let cu_price_ix =
+                                ComputeBudgetInstruction::set_compute_unit_price(self.priority_fee);
+                            let mut final_ixs = vec![];
+                            final_ixs.extend_from_slice(&[cu_budget_ix, cu_price_ix]);
+                            final_ixs.extend_from_slice(ixs);
+                            tx = Transaction::new_with_payer(&final_ixs, Some(&signer.pubkey()));
+                            break 'simulate;
+                        }
+                    }
+                    Err(err) => {
+                        println!("Simulaton error: {:?}", err);
+                        sim_attempts += 1;
+                        if sim_attempts.gt(&SIMULATION_RETRIES) {
+                            return Err(ClientError {
+                                request: None,
+                                kind: ClientErrorKind::Custom("Simulation failed".into()),
+                            });
+                        }
+                    }
                 }
             }
         }
