a9ace5c5ca2fbe11ecd131f3df0c012d3b74b1aa
Hardhat Chad <hardhatchad@gmail.com>
2024-04-12 21:36:13 +0000

    difficulty

--- src/update_difficulty.rs
+++ src/update_difficulty.rs
@@ -6,19 +6,13 @@ use crate::Miner;
 impl Miner {
     pub async fn update_difficulty(&self) {
         let signer = self.signer();
-        // let new_difficulty = KeccakHash::new_from_array([
-        //     0, 0, 0, 64, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-        //     255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-        // ]);
         let new_difficulty = KeccakHash::new_from_array([
-            0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            0, 0, 0, 16, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
         ]);
         let ix = ore::instruction::update_difficulty(signer.pubkey(), new_difficulty.into());
-        // let bs58data = bs58::encode(ix.data).into_string();
-        // println!("Data: {:?}", bs58data);
-        self.send_and_confirm(&[ix], false, false)
-            .await
-            .expect("Transaction failed");
+        println!("New difficulty: {:?}", new_difficulty.to_string());
+        let bs58data = bs58::encode(ix.data).into_string();
+        println!("Data: {:?}", bs58data);
     }
 }
