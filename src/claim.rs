26237324eda7243d1e0258bc15611aaa9bc49e6b
Hardhat Chad <hardhatchad@gmail.com>
2024-05-28 16:50:06 +0000

    fix claim

--- src/claim.rs
+++ src/claim.rs
@@ -1,7 +1,6 @@
 use std::str::FromStr;
 
 use colored::*;
-use ore::{self, ONE_DAY};
 use solana_program::pubkey::Pubkey;
 use solana_sdk::signature::Signer;
 use spl_token::amount_to_ui_amount;
@@ -10,7 +9,7 @@ use crate::{
     args::ClaimArgs,
     cu_limits::CU_LIMIT_CLAIM,
     send_and_confirm::ComputeBudget,
-    utils::{amount_f64_to_u64, ask_confirm, get_clock, get_proof},
+    utils::{amount_f64_to_u64, ask_confirm, get_proof},
     Miner,
 };
 
@@ -31,26 +30,14 @@ impl Miner {
             proof.balance
         };
 
-        // Burn warning
-        let clock = get_clock(&self.rpc_client).await;
-        let t = proof.last_claim_at.saturating_add(ONE_DAY);
-        if clock.unix_timestamp.lt(&t) {
-            let burn_amount = amount
-                .saturating_mul(t.saturating_sub(clock.unix_timestamp) as u64)
-                .saturating_div(ONE_DAY as u64);
-            let hours_ago =
-                (clock.unix_timestamp.saturating_sub(proof.last_claim_at) as f64) / 60f64 / 64f64;
-            let mins_to_go = t.saturating_sub(clock.unix_timestamp).saturating_div(60);
-            if !ask_confirm(
-                format!("\n{} You are about to burn {}!\nClaiming more frequently than once per day is subject to a burn penalty.\nYour last claim was {:.2} hours ago. You must wait {} minutes to avoid this penalty.\n\nAre you sure you want to continue? [Y/n]", 
-                    "WARNING".bold().yellow(),
-                    format!("{} ORE", amount_to_ui_amount(burn_amount, ore::TOKEN_DECIMALS)).bold(),
-                    hours_ago,
-                    mins_to_go
-                ).as_str()
-            ) {
-                return;
-            }
+        if !ask_confirm(
+            format!(
+                "\nYou are about to claim {}.\n\nAre you sure you want to continue? [Y/n]",
+                format!("{} ORE", amount_to_ui_amount(amount, ore::TOKEN_DECIMALS)).bold(),
+            )
+            .as_str(),
+        ) {
+            return;
         }
 
         let ix = ore::instruction::claim(pubkey, beneficiary, amount);
