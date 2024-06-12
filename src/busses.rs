51fbcd2ecbbf3f7dea76c3cc7827a0771f06fa10
Daniel Chrobak <danielchrobak12@gmail.com>
2024-06-11 22:01:16 -0500

    Update busses.rs
    use get_multiple_accounts instead of get_account_data to reduce API calls

--- src/busses.rs
+++ src/busses.rs
@@ -1,4 +1,9 @@
-use ore::{state::Bus, utils::AccountDeserialize, BUS_ADDRESSES, TOKEN_DECIMALS};
+use ore::{
+    state::Bus,
+    utils::AccountDeserialize,
+    BUS_ADDRESSES,
+    TOKEN_DECIMALS,
+};
 
 use crate::Miner;
 
@@ -8,18 +13,12 @@ impl Miner {
         let data = client.get_multiple_accounts(&BUS_ADDRESSES).await.unwrap();
 
         for (_address, account) in BUS_ADDRESSES.iter().zip(data.iter()) {
-            match account {
-                Some(account) => {
-                    let data_bytes = &account.data[..]; // Extract data bytes
-                    match Bus::try_from_bytes(data_bytes) {
-                        Ok(bus) => {
-                            let rewards = (bus.rewards as f64) / 10f64.powf(TOKEN_DECIMALS as f64);
-                            println!("Bus {}: {} ORE", bus.id, rewards);
-                        }
-                        Err(_) => {}
-                    }
+            if let Some(account) = account {
+                let data_bytes = &account.data[..]; // Extract data bytes
+                if let Ok(bus) = Bus::try_from_bytes(data_bytes) {
+                    let rewards = (bus.rewards as f64) / 10f64.powf(TOKEN_DECIMALS as f64);
+                    println!("Bus {}: {} ORE", bus.id, rewards);
                 }
-                None => {}
             }
         }
     }
