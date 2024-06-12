871f41028690c06a494701cb01fac3d4a78a3980
Daniel Chrobak <danielchrobak12@gmail.com>
2024-06-11 20:22:09 -0500

    Update busses.rs
    print the bus with most rewards

--- src/busses.rs
+++ src/busses.rs
@@ -1,26 +1,32 @@
 use ore::{state::Bus, utils::AccountDeserialize, BUS_ADDRESSES, TOKEN_DECIMALS};
-use solana_client::client_error::Result;
 
 use crate::Miner;
 
 impl Miner {
     pub async fn busses(&self) {
         let client = self.rpc_client.clone();
+        let mut max_bus: Option<Bus> = None;
+        let mut max_rewards = 0.0;
+
         for address in BUS_ADDRESSES.iter() {
             let data = client.get_account_data(address).await.unwrap();
             match Bus::try_from_bytes(&data) {
                 Ok(bus) => {
                     let rewards = (bus.rewards as f64) / 10f64.powf(TOKEN_DECIMALS as f64);
                     println!("Bus {}: {:} ORE", bus.id, rewards);
+                    if rewards > max_rewards {
+                        max_rewards = rewards;
+                        max_bus = Some(*bus);
+                    }
                 }
                 Err(_) => {}
             }
         }
-    }
 
-    pub async fn get_bus(&self, id: usize) -> Result<Bus> {
-        let client = self.rpc_client.clone();
-        let data = client.get_account_data(&BUS_ADDRESSES[id]).await?;
-        Ok(*Bus::try_from_bytes(&data).unwrap())
+        if let Some(bus) = max_bus {
+            let max_rewards = (bus.rewards as f64) / 10f64.powf(TOKEN_DECIMALS as f64);
+            println!("Bus with the most rewards: Bus {}: {:} ORE", bus.id, max_rewards);
+        }
     }
 }
+
