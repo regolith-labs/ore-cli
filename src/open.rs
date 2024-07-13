323ee3b6692e3153a360ccdc1142491fed399587
Hardhat Chad <hardhatchad@gmail.com>
2024-07-13 17:01:35 +0000

    update open command

--- src/open.rs
+++ src/open.rs
@@ -13,7 +13,7 @@ impl Miner {
 
         // Sign and send transaction.
         println!("Generating challenge...");
-        let ix = ore_api::instruction::open(signer.pubkey(), signer.pubkey());
+        let ix = ore_api::instruction::open(signer.pubkey(), signer.pubkey(), signer.pubkey());
         self.send_and_confirm(&[ix], ComputeBudget::Dynamic, false)
             .await
             .ok();
