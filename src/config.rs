f929e8f054400ccae21a5ba5593682823b180496
Hardhat Chad <hardhatchad@gmail.com>
2024-07-23 17:03:59 +0000

    update config printouts

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,20 @@
 use colored::Colorize;
 
-use crate::{utils::get_config, Miner};
+use crate::{
+    utils::{amount_u64_to_string, get_config},
+    Miner,
+};
 
 impl Miner {
     pub async fn config(&self) {
         let config = get_config(&self.rpc_client).await;
-        println!("{}: {}", "Last reset".bold(), config.last_reset_at);
+        println!("{}: {}", "Last reset at".bold(), config.last_reset_at);
         println!("{}: {}", "Min difficulty".bold(), config.min_difficulty);
         println!("{}: {}", "Base reward rate".bold(), config.base_reward_rate);
+        println!(
+            "{}: {} ORE",
+            "Top stake".bold(),
+            amount_u64_to_string(config.top_balance)
+        );
     }
 }
