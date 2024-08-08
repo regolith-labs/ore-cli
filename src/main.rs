4432fc5eddb74010d4a6cfe15f7b59dae6a1f5be
Hardhat Chad <hardhatchad@gmail.com>
2024-08-08 05:22:47 +0000

    use default keypair for fee payer

--- src/main.rs
+++ src/main.rs
@@ -161,7 +161,7 @@ async fn main() {
     // Initialize miner.
     let cluster = args.rpc.unwrap_or(cli_config.json_rpc_url);
     let default_keypair = args.keypair.unwrap_or(cli_config.keypair_path.clone());
-    let fee_payer_filepath = args.fee_payer.unwrap_or(cli_config.keypair_path.clone());
+    let fee_payer_filepath = args.fee_payer.unwrap_or(default_keypair.clone());
     let rpc_client = RpcClient::new_with_commitment(cluster, CommitmentConfig::confirmed());
 
     let miner = Arc::new(Miner::new(
