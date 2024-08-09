6c5ae4bc6eb13ffe882ec8a9e033ace8b4b0c9d3
Hardhat Chad <155858888+HardhatChad@users.noreply.github.com>
2024-08-09 09:59:37 -0500

    Update send_and_confirm.rs

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -63,15 +63,7 @@ impl Miner {
 
         // Set compute unit price
         final_ixs.push(ComputeBudgetInstruction::set_compute_unit_price(
-            if self.dynamic_fee {
-                if let Some(dynamic_fee) = self.dynamic_fee().await {
-                    dynamic_fee
-                }else{
-                    self.priority_fee.unwrap_or(0)
-                }
-            } else {
-                self.priority_fee.unwrap_or(0)
-            }        
+            self.priority_fee.unwrap_or(0),      
         ));
 
         // Add in user instructions
@@ -97,21 +89,17 @@ impl Miner {
             if attempts % 10 == 0 {
                 // Reset the compute unit price
                 if self.dynamic_fee {
-                    if let Some(dynamic_fee) = self.dynamic_fee().await {
-                        progress_bar.println(format!("  Priority fee: {} microlamports", dynamic_fee));
-                        final_ixs.remove(1);
-                        final_ixs.insert(1, ComputeBudgetInstruction::set_compute_unit_price(dynamic_fee));
+                    let fee = if let Some(fee) = self.dynamic_fee().await {
+                        progress_bar.println(format!("  Priority fee: {} microlamports", fee));
+                        fee
                     } else {
-                        let fallback_fee = self.priority_fee.unwrap_or(0);
-                        progress_bar.println(format!(
-                            "{} Dynamic fees not supported by this RPC. Falling back to static value: {} microlamports",
-                            "WARNING".bold().yellow(),
-                            fallback_fee
-                        ));
-                        final_ixs.remove(1);
-                        final_ixs.insert(1, ComputeBudgetInstruction::set_compute_unit_price(fallback_fee));
-                    }
-                }                
+                        let fee = self.priority_fee.unwrap_or(0);
+                        progress_bar.println(format!("  {} Dynamic fees not supported by this RPC. Falling back to static value: {} microlamports", "WARNING".bold().yellow(), fee));
+                        fee
+                    };
+                    final_ixs.remove(1);
+                    final_ixs.insert(1, ComputeBudgetInstruction::set_compute_unit_price(fee));
+                }           
 
                 // Resign the tx
                 let (hash, _slot) = client
