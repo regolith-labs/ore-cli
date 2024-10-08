39e991c83fb608dff6b30e5c7f328dd536f99d5d
big time tap in <bigtimetapin@gmail.com>
2024-10-08 17:12:24 -0300

    Pool Staking (#156)
    * integrate with ore boosts
    
    * initialize boost program
    
    * support for boosts
    
    * program id
    
    * drop boost init
    
    * fix stake pda deriv
    
    * manual cu budget ix
    
    * add payer to open
    
    * stake to pool impl; wip
    
    * stake to pool impl
    
    * register pool member on stake
    
    * register staker
    
    * pool unstake
    
    * Code TLC.
    
    * Code TLC.
    
    * Code TLC.
    
    * Code TLC.
    
    * Code TLC.
    
    ---------
    
    Co-authored-by: Hardhat Chad <hardhatchad@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -4,8 +4,12 @@ pub enum Error {
     Internal(String),
     #[error("reqwest")]
     Reqwest(#[from] reqwest::Error),
+    #[error("solana parse pubkey")]
+    SolanaParsePubkey(#[from] solana_sdk::pubkey::ParsePubkeyError),
     #[error("solana rpc client")]
     SolanaRpcClient(#[from] solana_client::client_error::ClientError),
     #[error("solana program")]
     SolanaProgram(#[from] solana_program::program_error::ProgramError),
+    #[error("parse int")]
+    ParseInt(#[from] std::num::ParseIntError),
 }
--- src/stake.rs
+++ src/stake.rs
@@ -1,24 +1,97 @@
 use std::str::FromStr;
 
-use solana_program::pubkey::Pubkey;
-use solana_sdk::signature::Signer;
+use ore_boost_api::state::{boost_pda, stake_pda, Boost, Stake};
+use ore_utils::AccountDeserialize;
+use solana_program::{program_pack::Pack, pubkey::Pubkey};
+use solana_sdk::{signature::Signer, transaction::Transaction};
+use spl_token::state::Mint;
 
 use crate::{
-    args::StakeArgs, cu_limits::CU_LIMIT_CLAIM, send_and_confirm::ComputeBudget,
-    utils::amount_f64_to_u64, Miner,
+    args::StakeArgs, cu_limits::CU_LIMIT_CLAIM, error::Error, pool::Pool,
+    send_and_confirm::ComputeBudget, Miner,
 };
 
 impl Miner {
     pub async fn stake(&self, args: StakeArgs) {
+        match args.pool_url {
+            None => {
+                self.stake_solo(&args).await;
+            }
+            Some(ref pool_url) => {
+                if let Err(err) = self.stake_pool(&args, pool_url).await {
+                    println!("{:?}", err);
+                }
+            }
+        }
+    }
+    async fn stake_pool(&self, args: &StakeArgs, pool_url: &String) -> Result<(), Error> {
+        let signer = self.signer();
+        // build pool client
+        let pool = Pool {
+            http_client: reqwest::Client::new(),
+            pool_url: pool_url.clone(),
+        };
+        // register member, if needed
+        let _ = pool.post_pool_register(self).await?;
+        // fetch pool address
+        let pool_address = pool.get_pool_address().await?;
+        // parse mint
+        let mint = Pubkey::from_str(args.mint.as_str())?;
+        // get sender token account
+        let sender = match &args.token_account {
+            Some(address) => Pubkey::from_str(address.as_str())?,
+            None => {
+                spl_associated_token_account::get_associated_token_address(&signer.pubkey(), &mint)
+            }
+        };
+        // assert that sender exists
+        let Ok(Some(token_account)) = self.rpc_client.get_token_account(&sender).await else {
+            println!("Failed to fetch token account");
+            return Err(Error::Internal(
+                "sender token account does not exist".to_string(),
+            ));
+        };
+        // assert that mint exists
+        let mint_data = self.rpc_client.get_account_data(&mint).await?;
+        let mint_account = Mint::unpack(&mint_data)?;
+        // parse amount
+        let amount: u64 = if let Some(amount) = args.amount {
+            (amount * 10f64.powf(mint_account.decimals as f64)) as u64
+        } else {
+            u64::from_str(token_account.token_amount.amount.as_str())?
+        };
+        // derive pdas
+        let boost_address = boost_pda(mint).0;
+        let stake_address = stake_pda(pool_address.address, boost_address).0;
+        // assert that boost exists
+        let boost_data = self.rpc_client.get_account_data(&boost_address).await?;
+        let _ = Boost::try_from_bytes(boost_data.as_slice())?;
+        // assert that stake exists (belongs to pool account)
+        let stake_data = self.rpc_client.get_account_data(&stake_address).await?;
+        let _ = Stake::try_from_bytes(stake_data.as_slice())?;
+        // open share account, if needed
+        let _ = pool.post_pool_register_staker(self, &mint).await?;
+        // send tx
+        let ix =
+            ore_pool_api::sdk::stake(signer.pubkey(), mint, pool_address.address, sender, amount);
+        let _ = self
+            .send_and_confirm(&[ix], ComputeBudget::Fixed(CU_LIMIT_CLAIM), false)
+            .await?;
+        Ok(())
+    }
+    async fn stake_solo(&self, args: &StakeArgs) {
+        // Parse mint address
+        let mint_address = Pubkey::from_str(&args.mint).unwrap();
+
         // Get signer
         let signer = self.signer();
-        let sender = match args.token_account {
+        let sender = match &args.token_account {
             Some(address) => {
                 Pubkey::from_str(&address).expect("Failed to parse token account address")
             }
             None => spl_associated_token_account::get_associated_token_address(
                 &signer.pubkey(),
-                &ore_api::consts::MINT_ADDRESS,
+                &mint_address,
             ),
         };
 
@@ -28,16 +101,42 @@ impl Miner {
             return;
         };
 
+        let Ok(mint_data) = self.rpc_client.get_account_data(&mint_address).await else {
+            println!("Failed to fetch mint address");
+            return;
+        };
+        let mint = Mint::unpack(&mint_data).unwrap();
+
         // Parse amount
         let amount: u64 = if let Some(amount) = args.amount {
-            amount_f64_to_u64(amount)
+            (amount * 10f64.powf(mint.decimals as f64)) as u64
         } else {
             u64::from_str(token_account.token_amount.amount.as_str())
                 .expect("Failed to parse token balance")
         };
 
+        // Get addresses
+        let boost_address = boost_pda(mint_address).0;
+        let stake_address = stake_pda(signer.pubkey(), boost_address).0;
+
+        // Fetch boost
+        let Ok(boost_account_data) = self.rpc_client.get_account_data(&boost_address).await else {
+            println!("Failed to fetch boost account");
+            return;
+        };
+        let _ = Boost::try_from_bytes(&boost_account_data).unwrap();
+
+        // Open stake account, if needed
+        if let Err(_err) = self.rpc_client.get_account_data(&stake_address).await {
+            println!("Failed to fetch stake account");
+            let ix = ore_boost_api::sdk::open(signer.pubkey(), signer.pubkey(), mint_address);
+            self.send_and_confirm(&[ix], ComputeBudget::Fixed(CU_LIMIT_CLAIM), false)
+                .await
+                .ok();
+        }
+
         // Send tx
-        let ix = ore_api::instruction::stake(signer.pubkey(), sender, amount);
+        let ix = ore_boost_api::sdk::deposit(signer.pubkey(), mint_address, amount);
         self.send_and_confirm(&[ix], ComputeBudget::Fixed(CU_LIMIT_CLAIM), false)
             .await
             .ok();
