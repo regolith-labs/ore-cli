cf16b2766b91a1afc27c32066a9205b8ddfc42bd
alex <aabinaei@gmail.com>
2024-10-14 06:20:58 -0700

    pool balance

--- src/balance.rs
+++ src/balance.rs
@@ -2,17 +2,66 @@ use std::str::FromStr;
 
 use solana_program::pubkey::Pubkey;
 use solana_sdk::signature::Signer;
+use steel::AccountDeserialize;
 
 use crate::{
     args::BalanceArgs,
-    utils::{amount_u64_to_string, get_proof_with_authority},
+    error::Error,
+    pool::Pool,
+    utils::{self, amount_u64_to_string, get_proof_with_authority},
     Miner,
 };
 
 impl Miner {
     pub async fn balance(&self, args: BalanceArgs) {
+        match args.pool_url {
+            None => {
+                self.balance_solo(&args).await;
+            }
+            Some(ref pool_url) => {
+                if let Err(err) = self.balance_pool(pool_url).await {
+                    println!("{:?}", err);
+                }
+            }
+        }
+    }
+    async fn balance_pool(&self, pool_url: &String) -> Result<(), Error> {
         let signer = self.signer();
-        let address = if let Some(address) = args.address {
+        // build pool client
+        let pool = Pool {
+            http_client: reqwest::Client::new(),
+            pool_url: pool_url.clone(),
+        };
+        // fetch pool address
+        let pool_address = pool.get_pool_address().await?;
+        // fetch on-chain balance
+        let (member_pda, _) =
+            ore_pool_api::state::member_pda(signer.pubkey(), pool_address.address);
+        let member_data = self.rpc_client.get_account_data(&member_pda).await?;
+        let member = ore_pool_api::state::Member::try_from_bytes(member_data.as_slice())?;
+        println!("//////////////////////////////");
+        println!(
+            "your on-chain pool balance: {:?}",
+            utils::amount_u64_to_string(member.balance)
+        );
+        // fetch db balance
+        let member_db = pool.get_pool_member(&self).await?;
+        let diff = (member_db.total_balance as u64) - member.total_balance;
+        if diff.gt(&0) {
+            println!("//////////////////////////////");
+            println!(
+                "you have an amount pending on-chain attribution: {:?}",
+                utils::amount_u64_to_string(diff)
+            );
+            println!("the pool operator automatically attributes your on-chain balance at regular intervals.");
+            println!("if you want to attribute this balance yourself now, you can pay the transaction fee by running the 'ore update-pool-balance {}' command.", pool_url);
+            println!("for more info run the 'ore help' command.");
+        }
+        Ok(())
+    }
+    async fn balance_solo(&self, args: &BalanceArgs) {
+        let signer = self.signer();
+        let address = if let Some(address) = &args.address {
             if let Ok(address) = Pubkey::from_str(&address) {
                 address
             } else {
