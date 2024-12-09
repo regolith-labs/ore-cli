bedefb58918ef01c515c8468c197127a21808a43
Hardhat Chad <hardhatchad@gmail.com>
2024-12-09 14:27:21 -0800

    fetch leaderboard

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
 use colored::Colorize;
 use ore_api::consts::EPOCH_DURATION;
+use ore_boost_api::state::{leaderboard_pda, Leaderboard};
+use steel::AccountDeserialize;
 
 use crate::{utils::get_config, Miner};
 
@@ -10,5 +12,20 @@ impl Miner {
         println!("{}: {}", "Min difficulty".bold(), config.min_difficulty);
         println!("{}: {}", "Base reward rate".bold(), config.base_reward_rate);
         println!("{}: {} sec", "Epoch time".bold(), EPOCH_DURATION);
+
+        // Print leaderboard
+        let leaderboard_pda = leaderboard_pda();
+        match self.rpc_client.get_account(&leaderboard_pda.0).await {
+            Ok(account) => {
+                let leaderboard = Leaderboard::try_from_bytes(&account.data).unwrap();
+                println!("\n{}", "Leaderboard:".bold());
+                for (i, entry) in leaderboard.entries.iter().enumerate() {
+                    if entry.score > 0 {
+                        println!("{}. {} - Score: {}", i + 1, entry.address, entry.score);
+                    }
+                }
+            }
+            Err(_) => println!("Could not fetch leaderboard data")
+        }
     }
 }
