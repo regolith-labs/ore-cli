51cefbfa283c953b37a72a73ce297665136be449
Hardhat Chad <hardhatchad@gmail.com>
2024-12-22 14:25:31 -0600

    log reservation boost

--- src/proof.rs
+++ src/proof.rs
@@ -1,31 +1,35 @@
 use std::str::FromStr;
 
 use ore_api::consts::TOKEN_DECIMALS;
+use ore_boost_api::state::reservation_pda;
 use solana_program::pubkey::Pubkey;
 use solana_sdk::signature::Signer;
 use spl_token::amount_to_ui_amount;
 
 use crate::{
     args::ProofArgs,
-    utils::{get_proof, proof_pubkey},
+    utils::{get_proof, proof_pubkey, get_reservation},
     Miner,
 };
 
 impl Miner {
     pub async fn proof(&self, args: ProofArgs) {
         let signer = self.signer();
-        let address = if let Some(address) = args.address {
+        let proof_address = if let Some(address) = args.address {
             Pubkey::from_str(&address).unwrap()
         } else {
             proof_pubkey(signer.pubkey())
         };
-        let proof = get_proof(&self.rpc_client, address).await;
-        println!("Address: {:?}", address);
+        let reservation_address = reservation_pda(proof_address).0;
+        let proof = get_proof(&self.rpc_client, proof_address).await;
+        let reservation = get_reservation(&self.rpc_client, reservation_address).await.unwrap();
+        println!("Address: {:?}", proof_address);
         println!("Authority: {:?}", proof.authority);
         println!(
             "Balance: {:?} ORE",
             amount_to_ui_amount(proof.balance, TOKEN_DECIMALS)
         );
+        println!("Boost: {:?}", reservation.boost);
         println!(
             "Last hash: {}",
             solana_sdk::hash::Hash::new_from_array(proof.last_hash).to_string()
