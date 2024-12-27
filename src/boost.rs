087ea519cc70b49197a93f02ac2a26edd1be9f40
Hardhat Chad <hardhatchad@gmail.com>
2024-12-27 10:18:55 -0600

    boost proof balance

--- src/boost.rs
+++ src/boost.rs
@@ -1,10 +1,11 @@
 use std::str::FromStr;
 
-use ore_boost_api::{state::{boost_pda, Boost}, consts::BOOST_DENOMINATOR};
+use ore_api::state::proof_pda;
+use ore_boost_api::{state::boost_pda, consts::BOOST_DENOMINATOR};
 use solana_client::client_error::Result as ClientResult;
 use steel::*;
 
-use crate::{args::BoostArgs, Miner, utils::get_boosts};
+use crate::{args::BoostArgs, Miner, utils::{get_boosts, get_boost, get_proof}};
 
 impl Miner {
     pub async fn boost(&self, args: BoostArgs) -> ClientResult<()> {
@@ -19,16 +20,16 @@ impl Miner {
 
     async fn lookup_boost(&self, mint: Pubkey) {
         let boost_address = boost_pda(mint).0;
-        let Ok(data) = self.rpc_client.get_account_data(&boost_address).await else {
-            println!("No boost found for mint {:?}", mint);
-            return;
-        };
-        let boost = Boost::try_from_bytes(&data).unwrap();
+        let boost = get_boost(&self.rpc_client, boost_address).await;
+        let proof_address = proof_pda(boost_address).0;
+        let proof = get_proof(&self.rpc_client, proof_address).await;
+        println!("{:?} ({:?})", boost.mint, boost.multiplier as f64 / BOOST_DENOMINATOR as f64);
         println!("Address: {:?}", boost_address);
         println!("Expires at: {:?}", boost.expires_at);
         println!("Mint: {:?}", mint);
         println!("Multiplier: {:?}", boost.multiplier);
         println!("Total stake: {:?}", boost.total_stake);
+        println!("Pending yield: {:?}", proof.balance);
     }
 
     async fn list_boosts(&self) {
