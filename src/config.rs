4081855422f900445f6bcc67db5effe1fcb67a5d
Hardhat Chad <hardhatchad@gmail.com>
2024-12-27 10:50:36 -0600

    format

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,14 @@
 use colored::Colorize;
 use ore_api::consts::EPOCH_DURATION;
 
-use crate::{utils::get_config, Miner};
+use crate::{utils::{get_config, amount_u64_to_f64}, Miner};
 
 impl Miner {
     pub async fn config(&self) {
         let config = get_config(&self.rpc_client).await;
         println!("{}: {}", "Last reset at".bold(), config.last_reset_at);
-        println!("{}: {}", "Min difficulty".bold(), config.min_difficulty);
-        println!("{}: {}", "Base reward rate".bold(), config.base_reward_rate);
-        println!("{}: {} sec", "Epoch time".bold(), EPOCH_DURATION);
+        println!("{}: {}", "Minimum difficulty".bold(), config.min_difficulty);
+        println!("{}: {} ORE", "Base reward rate".bold(), amount_u64_to_f64(config.base_reward_rate));
+        println!("{}: {} sec", "Epoch duration".bold(), EPOCH_DURATION);
     }
 }
